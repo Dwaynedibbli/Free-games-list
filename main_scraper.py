e24c5499e5e5e3d8aeb385482a14c58d6bcfcfad
Dwaynedibbli <geoffknight83@gmail.com>
2024-10-16 20:37:59 -0300

    Update main_scraper.py

--- main_scraper.py
+++ main_scraper.py
@@ -1,12 +1,14 @@
-from steam_scraper import scrape_steam
-from steam_scraper import save_to_file
+from steam_scraper import scrape_steam, save_to_file
+from gog_scraper import scrape_gog
 
 if __name__ == "__main__":
     steam_games = scrape_steam()
+    gog_games = scrape_gog()
 
-    # Create a dictionary with Steam games
+    # Create a dictionary with both platforms
     games_by_platform = {
-        "Steam": steam_games
+        "Steam": steam_games,
+        "GOG": gog_games
     }
 
     # Save the scraped data to index.html
