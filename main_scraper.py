c5c35596e20c5aa08840a4f9b787a3f00c7f4d0a
Dwaynedibbli <geoffknight83@gmail.com>
2024-10-16 20:30:47 -0300

    Create main_scraper.py

---
+++ main_scraper.py
@@ -0,0 +1,13 @@
+from steam_scraper import scrape_steam
+from steam_scraper import save_to_file
+
+if __name__ == "__main__":
+    steam_games = scrape_steam()
+
+    # Create a dictionary with Steam games
+    games_by_platform = {
+        "Steam": steam_games
+    }
+
+    # Save the scraped data to index.html
+    save_to_file(games_by_platform)
