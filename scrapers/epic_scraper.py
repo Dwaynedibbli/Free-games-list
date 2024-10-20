e0acacc1a0b700a78c89621e98d09749b90f4e11
Dwaynedibbli <geoffknight83@gmail.com>
2024-10-19 22:46:12 -0300

    Update epic_scraper.py

--- scrapers/epic_scraper.py
+++ scrapers/epic_scraper.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -48,10 +47,10 @@ def scrape_epic():
         try:
             link_element = game.find_element(By.TAG_NAME, 'a')
             title_element = game.find_element(By.TAG_NAME, 'h6')
-            status_elements = game.find_elements(By.CLASS_NAME, 'css-gyjcm9')  # The element that may contain "Coming Soon"
+            status_element = game.find_element(By.CLASS_NAME, 'css-gyjcm9')  # The element that may contain "Coming Soon"
 
             # Skip games that are "Coming Soon"
-            if any(status_element.text.strip() == "Coming Soon" for status_element in status_elements):
+            if status_element and "Coming Soon" in status_element.text.strip():
                 continue
 
             if link_element and title_element:
